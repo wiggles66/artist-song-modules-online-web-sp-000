4bdd9a6e06015be0cd10045e7daba8b8084f2b5c
Michael Waide <mvwaide@gmail.com>
2020-07-28 17:09:52 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,16 +1,13 @@
 
 module Memorable
-   module ClassMethods
-   end
-
-   module InstanceMethods
-   end
-
   def reset_all
-    self.all.clear
-  end
 
-  def count
-    self.all.count
-  end
+        self.all.clear
+    end
+
+     def count
+       self.all.count
+    end
+
+
 end
